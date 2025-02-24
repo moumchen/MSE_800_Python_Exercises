0da43c10e8afefac57de08f15e58f6ee37dc23f2
Yanqian Chen <270595725@yoobeestudent.ac.nz>
2025-02-24 14:58:22 +1300

    feat: add activities for flask

---
+++ Week12/static/script.js
@@ -0,0 +1,8 @@
+document.getElementById('alertBtn').addEventListener('click', function() {
+    alert('Hello from JavaScript!');
+});
+
+document.getElementById("goToNewPage").addEventListener("click", function() {
+    window.location.href = "/new";
+});
+
